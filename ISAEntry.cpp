7a06af2f96bcd0f61f8eb9fa87427b6dd7ac552c
razvanspataru05 <razvanspataru38@gmail.com>
2026-03-19 11:11:28 +0200

    Finished ISA entry for the help button.

--- ISAEntry.cpp
+++ ISAEntry.cpp
@@ -9,12 +9,13 @@ struct ISAEntry
 };
 
 static const ISAEntry isaTable[] = {
+
     {"NOP", 0x00, 1, "No operation"},
 
     /* Load / Store instructions */
     {"LDA_IM", 0x01, 2, "Loads immediate value into register A"},
     {"LDA_DIR", 0x02, 3, "Loads value at immediate memory address into register A"},
-    {"LDA_REG", 0x03, 2, "Loads value from another register into register A"},
+    {"LDA_REG", 0x03, 2, "Loads value into register A"},
     {"STA_DIR", 0x04, 3, "Writes value of register A at memory address specified"},
     {"LDB_IM", 0x05, 2, "Loads immediate value into register B" },
     {"LDB_DIR", 0x06, 3, "Loads value at immediate memory address into register B"},
@@ -29,8 +30,53 @@ static const ISAEntry isaTable[] = {
     {"LDD_REG", 0x0F, 2, "Loads value from another register into register D"},
     {"STD_DIR", 0x10, 3, "Writes value of register D at memory address specified"},
 
-    /* Logical operations */
+    /* Arithmetic operations */
     {"ADD_IM", 0x20, 2, "Adds immediate value to register A. Updates flags"},
     {"ADD_REG", 0x21, 2, "Adds another register value to register A. Updates flags"},
-    {"ADC_IM"}
+    {"ADC_IM", 0x22, 2, "Adds value at specified memory address to register A. Updates flags"},
+    {"ADC_REG", 0x23, 2, "Adds another register value to register A and checks for carry flag. Updates flags"},
+    {"SUB_IM", 0x24, 2, "Subtracts immediate value from register A. Updates flags"},
+    {"SUB_REG", 0x25, 2, "Subtracts another register value from register A. Updates flags"},
+    {"SBC_IM", 0x26, 2, "Subtracts immediate value from register A and checks for carry flag. Updates flags"},
+    {"SBC_REG", 0x27, 2, "Subtracts another register value from register A and checks for carry flag. Updates flags"},
+    {"INC", 0x28, 2, "Increments specified register by 1. Updates flags"},
+    {"DEC", 0x29, 2, "Decrements specified register by 1. Updates flags"},
+    {"MUL_IM", 0x2A, 2, "Multiplies register A by immediate value. Updates flags"},
+    {"MUL_REG", 0x2B, 2, "Multiplies another register value by register A. Updates flags"},
+
+    /* Logical operations */
+    {"AND_IM", 0x30, 2, "Applies logical AND between immediate value and register A. Updates flags"},
+    {"AND_REG", 0x31, 2, "Applies logical AND between specified register and register A. Updates flags"},
+    {"OR_IM", 0x32, 2, "Applies logical OR between immediate value and register A. Updates flags"},
+    {"OR_REG", 0x33, 2, "Applies logical OR between specified register and register A. Updates flags"},
+    {"XOR_IM", 0x34, 2, "Applies logical XOR between immediate value and register A. Updates flags"},
+    {"XOR_REG", 0x35, 2, "Applies logical XOR between specified register and register A. Updates flags"},
+    {"NOT", 0x36, 2, "Applies the NOT operator on a specified register. Updates flags"},
+    {"SHL", 0x37, 2, "Shifts a specified register to the left by 1 position. Updates flags"},
+    {"SHR", 0x38, 2, "Shifts a specified register to the right by 1 position. Updates flags"},
+    
+    /* Compare instructions */
+    {"CMP_IM", 0x40, 2, "Compares immediate value to register A. Updates flags"},
+    {"CMP_REG", 0x41, 2, "Compares specified register value to register A. Updates flags"},
+    
+    /* Jump/Branch instructions */
+    {"JMP", 0x50, 3, "Computes 16-bit address. Assigns the address to PC"},
+    {"JZ", 0x51, 3, "Computes 16-bit address. Assigns the address to PC if Zero Flag is true"},
+    {"JNZ", 0x52, 3, "Computes 16-bit address. Assigns the address to PC if Zero Flag is NOT true"},
+    {"JC", 0x53, 3, "Computes 16-bit address. Assigns the address to PC if Carry Flag is true"},
+    {"JNC", 0x54, 3, "Computes 16-bit address. Assigns the address to PC if Carry Flag is NOT true"},
+    {"JN", 0x55, 3, "Computes 16-bit address. Assigns the address to PC if Negative Flag is true"},
+    {"JNN", 0x56, 3, "Computes 16-bit address. Assigns the address to PC if Negative Flag is NOT true"},
+    {"JO", 0x57, 3, "Computes 16-bit address. Assigns the address to PC if Overflow Flag is true"},
+    {"JNO", 0x58, 3, "Computes 16-bit address. Assigns the address to PC if Overflow Flag is NOT true"},
+
+    /* Stack instructions */
+    {"PUSH", 0x60, 2, "Assigns the value of a specified register to the memory address the SP points. Decrements SP"},
+    {"POP", 0x61, 2, "Assigns to a specified register the value of the memory addres the SP points. Increments SP"},
+    {"CALL", 0x62, 3, "Jumps to subroutines at given address, saving return address on the stack"},
+    {"RET", 0x63, 1, "Assigns the return address to PC"},
+
+    /* Misc */
+    {"MOV", 0x70, 3, "Copies the value of a register to another register"},
+    {"HLT", 0XFF, 1, "Stops the execution of the program"}
 };
